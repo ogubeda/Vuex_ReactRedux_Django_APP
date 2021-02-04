16d9cd0d65340f35003f24c40fd82e81ce39992f
rmenendezdaw <rmenendeziestacio@gmail.com>
2021-02-04 19:35:20 +0100

    List Details in progress

---
+++ frontend/vue/src/common/api.service.ts
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+import { Song } from '../models/Song';
+
+const api = 'api';
+
+class SongService {
+  constructor() {
+    console.log('creating new instance of song.service');
+  }
+
+  deleteSong(song: Song) {
+    return axios.delete(`${api}/songs/${song.slug}`);
+  }
+  getSonges() {
+    return axios.get<Song[]>(`${api}/songs`);
+  }
+  addSong(song: Song) {
+    return axios.post(`${api}/songs/`, { song });
+  }
+  updateSong(song: Song) {
+    return axios.put(`${api}/songs/${song.slug}`, { song });
+  }
+}
+
+// Export a singleton instance in the global namespace
+export const songService = new SongService();
---
+++ frontend/vue/src/models/Song.ts
@@ -0,0 +1,3 @@
+export class Song {
+    constructor(public slug: string, public title: string, public releaseDate: number, public genre: string, public duration: number, public album: string) {}
+  }
