271400047ec12d32ef925f9910b09829e02c90ee
ogubeda <og.ubeda@gmail.com>
2021-02-07 16:18:10 +0100

    Cambios en css

--- frontend/vue/src/components/songs-list/SongsPreview.vue
+++ frontend/vue/src/components/songs-list/SongsPreview.vue
@@ -1,9 +1,16 @@
 <template>
-<div>
-  <h2>{{ song.title }}</h2>
+<div class = "song_flex">
+    <div class="inSong">
+      <div class="inSongheader">
+        <img src="https://pbs.twimg.com/profile_images/728677222794592257/u-_opbnI.jpg" alt="icon song">
+      </div>
+      <div class="inSongbody">
+        <div class="songTitle">{{song.title}}</div>
+        <div class="songAlbum">{{song.album}}</div>
+      </div>
   </div>
+</div>
 </template>
-
 <script lang = "ts">
 
 import { defineComponent } from 'vue';
@@ -15,3 +22,32 @@ export default defineComponent({
     }
 });
 </script>
+
+
+<style>
+.song_flex {
+  flex: 15%;
+  max-width: 150px;
+}
+.songTitle{
+  color: white;
+  font-weight: bold;
+}
+.songAlbum{
+  color: lightgray;
+  font-weight: italic;
+
+}
+img{
+  width: 9em;
+  height: 8em;
+  border-radius:5%;
+}
+
+.inSongbody{
+  font-size: 15px;
+}
+.inSongheader{
+  text-align: center;
+}
+</style>
