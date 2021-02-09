c4da08ee9d5cd8172a9e8628b62e7362abc4aded
ogubeda <og.ubeda@gmail.com>
2021-02-09 16:16:17 +0100

    Implemetación users y profile

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
 
     'songs',
     'core',
-    # 'authentication'
+    'authentication',
+    'profiles'
 
 ]
 
@@ -140,11 +141,13 @@ CORS_ORIGIN_WHITELIST = (
     'http://0.0.0.0:8080'
 )
 
-# AUTH_USER_MODEL = 'authentication.User'
+AUTH_USER_MODEL = 'authentication.User'
 
 
 REST_FRAMEWORK = {
     'EXCEPTION_HANDLER': 'core.exceptions.core_exception_handler',
     'NON_FIELD_ERRORS_KEY': 'error',
-
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'authentication.backends.JWTAuthentication',
+    ),
 }
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -19,8 +19,7 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
-
+    url(r'^api/', include('authentication.urls', namespace='authentication')),
     url(r'^api/', include('songs.urls', namespace='songs')),
-
-
+    url(r'^api/', include('profiles.urls', namespace='profiles')),
 ]
