42738a0ae92f61ba043604c2e73a222789817d9b
rmenendezdaw <rmenendeziestacio@gmail.com>
2021-02-15 00:24:05 +0100

    CSS login

--- frontend/vue/src/App.vue
+++ frontend/vue/src/App.vue
@@ -2,7 +2,9 @@
   <div>
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/login">Sign in</router-link>
+
     </div>
     <section class="wrapper">
       <NavbarComponent />
