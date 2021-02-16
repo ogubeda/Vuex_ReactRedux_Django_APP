3dcc872a2a544fd76268043d91b52ba3a8ee064d
ogubeda <og.ubeda@gmail.com>
2021-02-16 17:35:38 +0100

    Auth al recargar

--- frontend/vue/src/main.ts
+++ frontend/vue/src/main.ts
@@ -3,4 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import { ActionsType } from './store/actions.type';
+
+router.beforeEach((to, from, next) =>
+  Promise.all([store.dispatch(ActionsType.CHECK_AUTH)]).then(next)
+);
+
 createApp(App).use(store).use(router).mount('#app')
